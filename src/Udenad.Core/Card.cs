d19e6df98d36192bb9dc7f6040157ba6b5db1454
Roman Klimenko <romaklimenko@gmail.com>
2018-02-21 19:39:00 +0100

    don't limit the Fibonacci sequence

--- src/Udenad.Core/Card.cs
+++ src/Udenad.Core/Card.cs
@@ -1,36 +1,11 @@
 ﻿using System;
-using System.Linq;
 using MongoDB.Bson.Serialization.Attributes;
 
 namespace Udenad.Core
 {
     public class Card
     {
-        private static int[] Fibonacci => new []
-        {
-            0,      // impossible
-            1,      // 1st, 0 days
-            2,      // 2nd, 1 day
-            3,      // 3rd, 3 days
-            5,      // 4th, 6 days
-            8,      // 5th, 11 days
-            13,     // 6th, 19 days
-            21,     // 7th, 32 days => the card becomes mature
-            34,     // 8th, 53 days
-            55,     // 9th, 108 days
-            89,     // 10th, 197 days
-            144,    // 11th, 286 days
-            233,    // 12th
-            377,    // 13th
-            610,    // 14th
-            987,    // 15th
-            1597,   // 16th
-            2584,   // 17th
-            4181    // 18th
-        };
-
-        [BsonId]
-        public string Word { get; set; }
+        [BsonId] public string Word { get; set; }
 
         public string WordClass { get; set; }
         public string Inflection { get; set; }
@@ -50,9 +25,28 @@ namespace Udenad.Core
             if (NextDate != null && DateTime.Today < NextDate)
                 return;
 
-            Repetitions = !score ? 0 : Math.Min(Fibonacci.Length - 1, Repetitions + 1);
+            Repetitions = !score ? 0 : Repetitions + 1;
 
-            NextDate = DateTime.Now.Date.AddDays(Fibonacci[Repetitions]);
+            NextDate = DateTime.Now.Date.AddDays(Fibonacci(Repetitions));
+        }
+
+        private static int Fibonacci(int step)
+        {
+            //  step: 0, 1, 2, 3, 4, 5,  6,  7,  8,  9, 10, ...
+            // value: 0, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, ...
+            
+            if (step <= 3) return Math.Max(0, step); // 0, 1, 2, 3 ...
+
+            int first = 2, second = 3, result = 0;
+
+            for (var i = 4; i <= step; i++) // from step 4
+            {
+                result = first + second;
+                first = second;
+                second = result;
+            }
+
+            return result;
         }
     }
 }
