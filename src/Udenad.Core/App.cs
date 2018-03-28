6398c3aaab6d2c5cdd9ecccfffee767379c8db2f
Roman Klimenko <romaklimenko@gmail.com>
2018-03-28 17:51:59 +0200

    Make the limits even more smarter

--- src/Udenad.Core/App.cs
+++ src/Udenad.Core/App.cs
@@ -112,14 +112,7 @@ namespace Udenad.Core
 
         public static async Task SaveCardAsync(Card card)
         {
-            while ( card.NextDate != null &&
-                    card.Repetitions > 0 &&
-                    await CardsCollection.CountAsync(
-                        // maximum new cards in a day
-                        c => c.NextDate == card.NextDate && c.Repetitions < 7) >= 50)
-            {
-                card.NextDate = card.NextDate?.AddDays(1);
-            }
+            card = await UpdateNextDate(card);
 
             await CardsCollection
                 .ReplaceOneAsync(
@@ -131,6 +124,34 @@ namespace Udenad.Core
                     });
 
             await SaveCountsAsync();
+        }
+
+        private static async Task<Card> UpdateNextDate(Card card)
+        {
+            if(card.NextDate == null) return card;
+
+            const int MATURE_PER_DAY = 99;
+            const int YOUNG_PER_DAY = 50;
+
+            if(card.Repetitions > 6) // mature
+            {
+                while (await CardsCollection.CountAsync(c =>
+                           c.NextDate == card.NextDate &&
+                           c.Repetitions > 6) > MATURE_PER_DAY)
+                {
+                    card.NextDate = card.NextDate?.AddDays(1);
+                }
+            }
+            else if (card.Repetitions > 0) // young
+            {
+                while (await CardsCollection.CountAsync(c =>
+                           c.NextDate == card.NextDate &&
+                         !(c.Repetitions > 6)) > YOUNG_PER_DAY)
+                {
+                    card.NextDate = card.NextDate?.AddDays(1);
+                }
+            }
+            return card;
         }
 
         private static async Task SaveCountAsync(Count count) =>
